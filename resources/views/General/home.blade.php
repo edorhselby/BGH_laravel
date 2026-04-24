953b3308cfad18fc55787ace08becd32ce45ff59
wick007 <selby@Wick007.localdomain>
2026-04-24 03:28:26 +0000

    new change

--- resources/views/General/home.blade.php
+++ resources/views/General/home.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/home.css">
-    @vite('resources/css/home.css')
+    {{-- @vite('resources/css/home.css') --}}
 </head>
 <body>
     <h1>WELCOME TO BEE'S GLAM HUB<br><br>LANDING PAGE</h1>
